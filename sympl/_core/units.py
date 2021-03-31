5fbe8e86052ac74e80356da4c4d86ffbd1519a4f
Stefano Ubbiali <subbiali@phys.ethz.ch>
2021-03-31 16:23:32 +0200

    Clean units.

--- sympl/_core/units.py
+++ sympl/_core/units.py
@@ -30,8 +30,11 @@
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 import functools
-
 import pint
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from sympl._core.typing import DataArray, NDArrayLike, PropertyDict
 
 
 class UnitRegistry(pint.UnitRegistry):
@@ -53,7 +56,7 @@ unit_registry.define(
 unit_registry.define("percent = 0.01*count = %")
 
 
-def units_are_compatible(unit1, unit2):
+def units_are_compatible(unit1: str, unit2: str) -> bool:
     """
     Determine whether a unit can be converted to another unit.
 
@@ -74,7 +77,7 @@ def units_are_compatible(unit1, unit2):
         return False
 
 
-def units_are_same(unit1, unit2):
+def units_are_same(unit1: str, unit2: str) -> bool:
     """
     Compare two unit strings for equality.
 
@@ -91,48 +94,58 @@ def units_are_same(unit1, unit2):
     return unit_registry(unit1) == unit_registry(unit2)
 
 
-def clean_units(unit_string):
-    return str(unit_registry(unit_string).to_base_units().units)
+def clean_units(unit: str) -> str:
+    return str(unit_registry(unit).to_base_units().units)
 
 
-def is_valid_unit(unit_string):
+def is_valid_unit(unit: str) -> bool:
     """Returns True if the unit string is recognized, and False otherwise."""
-    unit_string = unit_string.replace("%", "percent").replace("°", "degree")
+    unit = unit.replace("%", "percent").replace("°", "degree")
     try:
-        unit_registry(unit_string)
+        unit_registry(unit)
     except pint.UndefinedUnitError:
         return False
     else:
         return True
 
 
-def data_array_to_units(value, units):
-    if not hasattr(value, "attrs") or "units" not in value.attrs:
-        raise TypeError(
-            "Cannot retrieve units from type {}".format(type(value))
-        )
-    elif unit_registry(value.attrs["units"]) != unit_registry(units):
-        out = value.copy()
+def data_array_to_units(
+    data_array: "DataArray", units: str, *, enable_checks: bool = True
+) -> "DataArray":
+    if enable_checks:
+        if not hasattr(data_array, "attrs") or "units" not in data_array.attrs:
+            raise TypeError(
+                "Cannot retrieve units from type {}".format(type(data_array))
+            )
+
+    if unit_registry(data_array.attrs["units"]) != unit_registry(units):
+        out = data_array.copy()
         out.data[...] = (
-            unit_registry.convert(1, value.attrs["units"], units) * value.data
+            unit_registry.convert(1, data_array.attrs["units"], units)
+            * data_array.data
         )
         out.attrs["units"] = units
-        value = out
-    return value
+        data_array = out
+
+    return data_array
 
 
-def from_unit_to_another(value, original_units, new_units):
-    return (unit_registry(original_units) * value).to(new_units).magnitude
+def from_unit_to_another(
+    array: "NDArrayLike", original_units: str, new_units: str
+) -> "NDArrayLike":
+    return (unit_registry(original_units) * array).to(new_units).magnitude
 
 
-def get_name_with_incompatible_units(properties1, properties2):
+def get_name_with_incompatible_units(
+    properties1: "PropertyDict", properties2: "PropertyDict"
+) -> Optional[str]:
     """
     If there are any keys shared by the two properties
     dictionaries which indicate units that are incompatible with one another,
     this returns such a key. Otherwise returns None.
     """
-    for name in set(properties1.keys()).intersection(properties2.keys()):
-        if not units_are_compatible(
+    for name in properties1:
+        if name in properties2 and not units_are_compatible(
             properties1[name]["units"], properties2[name]["units"]
         ):
             return name
@@ -140,22 +153,23 @@ def get_name_with_incompatible_units(properties1, properties2):
 
 
 def get_tendency_name_with_incompatible_units(
-    input_properties, tendency_properties
-):
+    input_properties: "PropertyDict", tendency_properties: "PropertyDict"
+) -> Optional[str]:
     """
     Returns False if there are any keys shared by the two properties
     dictionaries which indicate units that are incompatible with one another,
     and True otherwise (if there are no conflicting unit specifications).
     """
-    for name in set(input_properties.keys()).intersection(
-        tendency_properties.keys()
-    ):
-        if input_properties[name]["units"] == "":
-            expected_tendency_units = "s^-1"
-        else:
-            expected_tendency_units = input_properties[name]["units"] + " s^-1"
-        if not units_are_compatible(
-            expected_tendency_units, tendency_properties[name]["units"]
-        ):
-            return name
+    for name in input_properties:
+        if name in tendency_properties:
+            if input_properties[name]["units"] == "":
+                expected_tendency_units = "s^-1"
+            else:
+                expected_tendency_units = (
+                    input_properties[name]["units"] + " s^-1"
+                )
+            if not units_are_compatible(
+                expected_tendency_units, tendency_properties[name]["units"]
+            ):
+                return name
     return None
