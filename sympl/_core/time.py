e85564c4334c5d75ac0f1a3c90a6595748df2aca
Stefano Ubbiali <subbiali@phys.ethz.ch>
2021-04-30 10:57:53 +0200

    Implement Timer.

--- sympl/_core/time.py
+++ sympl/_core/time.py
@@ -29,8 +29,13 @@
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 #
+from dataclasses import dataclass, field
 from datetime import datetime as real_datetime
+import functools
 import numpy as np
+import time
+import timeit
+from typing import Dict, List, Optional
 
 try:
     import cftime as ct
@@ -50,6 +55,15 @@ try:
 except ImportError:
     ct = None
 
+try:
+    import cupy as cp
+
+    # cp = None
+except ImportError:
+    cp = None
+
+
+from sympl._core.data_array import DataArray
 from sympl._core.exceptions import DependencyError
 
 
@@ -131,3 +145,179 @@ def datetime64_to_datetime(dt64):
         1, "s"
     )
     return datetime.utcfromtimestamp(ts)
+
+
+@dataclass
+class Node:
+    label: str
+    parent: "Node" = None
+    children: Dict[str, "Node"] = field(default_factory=dict)
+    level: int = 0
+    tic: float = 0
+    total_calls: int = 0
+    total_runtime: float = 0
+
+
+class Timer:
+    active: List[str] = []
+    head: Optional[Node] = None
+    tree: Dict[str, Node] = {}
+
+    @classmethod
+    def start(cls, label: str) -> None:
+        # safe-guard
+        if label in cls.active:
+            return
+
+        # mark node as active
+        cls.active.append(label)
+
+        # insert timer in the tree
+        node_label = cls.active[0]
+        node = cls.tree.setdefault(node_label, Node(node_label))
+        for i, node_label in enumerate(cls.active[1:]):
+            node = node.children.setdefault(
+                node_label, Node(node_label, parent=node, level=i + 1)
+            )
+        cls.head = node
+
+        # tic
+        if cp is not None:
+            try:
+                cp.cuda.Device(0).synchronize()
+            except RuntimeError:
+                pass
+        # cls.head.tic = timeit.default_timer()
+        cls.head.tic = time.perf_counter()
+
+    @classmethod
+    def stop(cls, label: Optional[str] = None) -> None:
+        # safe-guard
+        if len(cls.active) == 0:
+            return
+
+        # only nested timers allowed!
+        label = label or cls.active[-1]
+        assert (
+            label == cls.active[-1]
+        ), f"Cannot stop {label} before stopping {cls.active[-1]}"
+
+        # toc
+        if cp is not None:
+            try:
+                cp.cuda.Device(0).synchronize()
+            except RuntimeError:
+                pass
+        # toc = timeit.default_timer()
+        toc = time.perf_counter()
+
+        # update statistics
+        cls.head.total_calls += 1
+        cls.head.total_runtime += toc - cls.head.tic
+
+        # mark node as not active
+        cls.active = cls.active[:-1]
+
+        # update head
+        cls.head = cls.head.parent
+
+    @classmethod
+    def reset(cls) -> None:
+        cls.active = []
+        cls.head = None
+
+        def cb(node):
+            node.total_calls = 0
+            node.total_runtime = 0
+
+        for root in cls.tree.values():
+            cls.traverse(cb, root)
+
+    @classmethod
+    def get_time(cls, label, units="ms") -> None:
+        nodes = cls.get_nodes_from_label(label)
+        assert len(nodes) > 0, f"{label} is not a valid timer identifier."
+
+        raw_time = functools.reduce(
+            lambda x, node: x + node.total_runtime, nodes, 0
+        )
+        time = (
+            DataArray(raw_time, attrs={"units": "s"})
+            .to_units(units)
+            .data.item()
+        )
+
+        return time
+
+    @classmethod
+    def print(cls, label, units="ms") -> None:
+        time = cls.get_time(label, units)
+        print(f"{label}: {time:.3f} {units}")
+
+    @classmethod
+    def log(cls, logfile: str = "log.txt", units: str = "ms") -> None:
+        # ensure all timers have been stopped
+        assert len(cls.active) == 0, "Some timers are still running."
+
+        # callback
+        def cb(node, out, units, prefix="", has_peers=False):
+            level = node.level
+            prefix_now = prefix + "|- " if level > 0 else prefix
+            time = (
+                DataArray(node.total_runtime, attrs={"units": "s"})
+                .to_units(units)
+                .data.item()
+            )
+            out.write(f"{prefix_now}{node.label}: {time:.3f} {units}\n")
+
+            prefix_new = (
+                prefix
+                if level == 0
+                else prefix + "|  "
+                if has_peers
+                else prefix + "   "
+            )
+            peers_new = len(node.children)
+            has_peers_new = peers_new > 0
+            for i, label in enumerate(node.children.keys()):
+                cb(
+                    node.children[label],
+                    out,
+                    units,
+                    prefix=prefix_new,
+                    has_peers=has_peers_new and i < peers_new - 1,
+                )
+
+        # write to file
+        with open(logfile, "w") as outfile:
+            for root in cls.tree.values():
+                cb(root, outfile, units)
+
+    @staticmethod
+    def traverse(cb, node, **kwargs) -> None:
+        cb(node, **kwargs)
+        for child in node.children.values():
+            Timer.traverse(cb, child, **kwargs)
+
+    @classmethod
+    def get_nodes_from_label(cls, label) -> List[Node]:
+        out = []
+
+        def cb(node, out):
+            if node.label == label:
+                out.append(node)
+
+        for root in cls.tree.values():
+            Timer.traverse(cb, root, out=out)
+
+        return out
+
+
+class FakeTimer:
+    @classmethod
+    def start(cls, label: str) -> None:
+        pass
+
+    @classmethod
+    def stop(cls, label: Optional[str] = None) -> None:
+        pass
